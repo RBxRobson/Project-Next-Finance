3b774550590901e61ba14786152f9d39476a1e4f
RBxRobson <robsonnbarbosa0@gmail.com>
2024-04-06 17:22:51 -0300

    chore: Adiciona variáveis de cores e fontes

--- src/styles/themes.ts
+++ src/styles/themes.ts
@@ -1,11 +1,31 @@
 export default {
   colors: {
-    primary: 'tomato',
-    white: '#fff'
+    absoluteColors: {
+      white: '#fff',
+      black: '#000'
+    },
+    greenShades: {
+      g_60: '#CAFF33',
+      g_65: '#D1FF4C'
+    },
+    whiteShades: {
+      w_90: '#E4E4E7',
+      W_95: '#F1F1F3'
+    },
+    darkShades: {
+      d_10: '#191919',
+      d_11: '#1C1C1C'
+    }
   },
   spacings: {
     small: '1rem',
     medium: '2rem',
     large: '3rem'
+  },
+  fontSize: {
+    regular: 400,
+    medium: 500,
+    semi_bold: 600,
+    bold: 700
   }
 }
