4c5919fcc41b9333524fef5aed3190680eb5e394
RBxRobson <robsonnbarbosa0@gmail.com>
2024-04-06 17:24:49 -0300

    chore: Adiciona svg abstrato

--- src/styles/global-styles.ts
+++ src/styles/global-styles.ts
@@ -1,20 +1,40 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle, css } from 'styled-components'
+
+import lexendFont from '../assets/fonts/Lexend-VariableFont_wght.ttf'
+import abstract_svg from '../assets/images/abstract_design_1.svg'
 
 const GlobalStyles = createGlobalStyle`
-  html,
-  body {
-    padding: 0;
-    margin: 0;
+  @font-face {
+    font-family: lexend;
+    src: url(${lexendFont})
   }
 
-  a {
-    color: inherit;
-    text-decoration: none;
-  }
+  ${({ theme }) => css`
+    html,
+    body {
+      padding: 0;
+      margin: 0;
+      font-family: lexend, sans-serif;
+    }
 
-  * {
-    box-sizing: border-box;
-  }
+    body {
+      background-color: ${theme.colors.darkShades.d_10};
+      background-image: url(${abstract_svg});
+      background-repeat: no-repeat;
+      background-position: 0 -70px;
+      background-size: 800px 700px;
+      background-blend-mode: hard-light;
+    }
+
+    a {
+      color: inherit;
+      text-decoration: none;
+    }
+
+    * {
+      box-sizing: border-box;
+    }
+  `}
 `
 
 export default GlobalStyles
