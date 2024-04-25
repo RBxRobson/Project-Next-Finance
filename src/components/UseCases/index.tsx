a4c4128e5a0c4cdcbfbed05b6686574740a124ef
RBxRobson <robsonnbarbosa0@gmail.com>
2024-04-25 15:45:46 -0300

    chore: Adiciona novo conteúdo no componente UseCases

--- src/components/UseCases/index.tsx
+++ src/components/UseCases/index.tsx
@@ -1,5 +1,12 @@
 import * as S from './styles'
 
+import abstractLeft from '../../assets/images/design_radius_lf.svg'
+import abstractRight from '../../assets/images/design_radius_right.svg'
+import iconBank from '../../assets/images/icon_bank.png'
+import iconBook from '../../assets/images/icon_book.png'
+import iconMoneyBag from '../../assets/images/icon_money_bag.png'
+import iconSaveMoney from '../../assets/images/icon_save_money.png'
+
 const UseCases = () => {
   return (
     <S.UseCaseSection className="container">
@@ -10,6 +17,120 @@ const UseCases = () => {
         No Next Finance, atendemos às diversas necessidades de pessoas físicas e
         jurídicas, oferecendo uma ampla gama de soluções financeiras
       </p>
+      <S.UseCaseArticle>
+        <S.CardsWrapper>
+          <img src={abstractRight} className="position_left" />
+          <S.Card>
+            <img
+              src={iconSaveMoney}
+              alt="Ícone com mãos em formato de coração e um cifrão dentro "
+            />
+            <p>Gerenciamento de finanças pessoais</p>
+          </S.Card>
+          <S.Card>
+            <img
+              src={iconSaveMoney}
+              alt="Ícone com mãos em formato de coração e um cifrão dentro "
+            />
+            <p>Gerenciamento de finanças pessoais</p>
+          </S.Card>
+          <S.Card>
+            <img
+              src={iconSaveMoney}
+              alt="Ícone com mãos em formato de coração e um cifrão dentro "
+            />
+            <p>Gerenciamento de finanças pessoais</p>
+          </S.Card>
+          <S.Card>
+            <img
+              src={iconSaveMoney}
+              alt="Ícone com mãos em formato de coração e um cifrão dentro "
+            />
+            <p>Gerenciamento de finanças pessoais</p>
+          </S.Card>
+        </S.CardsWrapper>
+        <S.DescriptionContainer>
+          <h4>Para Pessoas</h4>
+          <p>
+            Nossos serviços de hipoteca abrem o caminho para a aquisição de uma
+            casa própria, e nossos empréstimos pessoais flexíveis fornecem
+            suporte vital durante vários marcos da vida. Também priorizamos o
+            planejamento da aposentadoria, garantindo um futuro financeiramente
+            seguro para nossos clientes
+          </p>
+          <S.StaticsWrapper>
+            <div>
+              <span>78%</span>
+              <p>Planejamento de aposentadoria</p>
+            </div>
+            <div>
+              <span>63%</span>
+              <p>Gerenciamento de dívidas</p>
+            </div>
+            <div>
+              <span>91%</span>
+              <p>Redução de encargos financeiros</p>
+            </div>
+          </S.StaticsWrapper>
+        </S.DescriptionContainer>
+      </S.UseCaseArticle>
+      <S.UseCaseArticle>
+        <S.DescriptionContainer>
+          <h4>Para Pessoas</h4>
+          <p>
+            Nossos serviços de hipoteca abrem o caminho para a aquisição de uma
+            casa própria, e nossos empréstimos pessoais flexíveis fornecem
+            suporte vital durante vários marcos da vida. Também priorizamos o
+            planejamento da aposentadoria, garantindo um futuro financeiramente
+            seguro para nossos clientes
+          </p>
+          <S.StaticsWrapper>
+            <div>
+              <span>78%</span>
+              <p>Planejamento de aposentadoria</p>
+            </div>
+            <div>
+              <span>63%</span>
+              <p>Gerenciamento de dívidas</p>
+            </div>
+            <div>
+              <span>91%</span>
+              <p>Redução de encargos financeiros</p>
+            </div>
+          </S.StaticsWrapper>
+        </S.DescriptionContainer>
+        <S.CardsWrapper>
+          <img src={abstractLeft} className="position_right" />
+          <S.Card>
+            <img
+              src={iconSaveMoney}
+              alt="Ícone com mãos em formato de coração e um cifrão dentro "
+            />
+            <p>Gerenciamento de finanças pessoais</p>
+          </S.Card>
+          <S.Card>
+            <img
+              src={iconSaveMoney}
+              alt="Ícone com mãos em formato de coração e um cifrão dentro "
+            />
+            <p>Gerenciamento de finanças pessoais</p>
+          </S.Card>
+          <S.Card>
+            <img
+              src={iconSaveMoney}
+              alt="Ícone com mãos em formato de coração e um cifrão dentro "
+            />
+            <p>Gerenciamento de finanças pessoais</p>
+          </S.Card>
+          <S.Card>
+            <img
+              src={iconSaveMoney}
+              alt="Ícone com mãos em formato de coração e um cifrão dentro "
+            />
+            <p>Gerenciamento de finanças pessoais</p>
+          </S.Card>
+        </S.CardsWrapper>
+      </S.UseCaseArticle>
     </S.UseCaseSection>
   )
 }
--- src/components/UseCases/styles.ts
+++ src/components/UseCases/styles.ts
@@ -1,19 +1,87 @@
 import styled, { css } from 'styled-components'
 
-export const UseCaseSection = styled.section``
+export const UseCaseSection = styled.section`
+  h2 {
+    padding-bottom: 14px;
+  }
+`
 
 export const UseCaseArticle = styled.article`
   display: flex;
+  gap: 100px;
+  padding-top: 80px;
+`
+
+export const CardsWrapper = styled.div`
+  ${({ theme }) => css`
+    position: relative;
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 20px;
+    padding: 50px;
+    background-color: ${theme.colors.darkShades.d_11};
+    border-radius: 20px;
+
+    & > img {
+      position: absolute;
+      z-index: 1;
+
+      &.position_left {
+        left: 0;
+        top: 0;
+      }
+
+      &.position_right {
+        right: 0;
+        top: 0;
+      }
+    }
+  `}
 `
 
 export const Card = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 20px;
-  padding: 50px;
+  ${({ theme }) => css`
+    z-index: 1;
+    width: 314px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 30px;
+    background-color: ${theme.colors.darkShades.d_10};
+    border-radius: 16px;
+
+    img {
+      width: 78px;
+      height: 78px;
+      margin-bottom: 24px;
+    }
+
+    p {
+      text-align: center;
+      font-size: ${theme.fontSize.normal};
+      font-weight: ${theme.fontWeight.regular};
+    }
+  `}
+`
+
+export const DescriptionContainer = styled.div`
+  ${({ theme }) => css`
+    h4 {
+      font-size: ${theme.fontSize.large};
+      margin-bottom: 14px;
+    }
+  `}
 `
 
 export const StaticsWrapper = styled.div`
-  display: flex;
-  gap: 50px;
+  ${({ theme }) => css`
+    display: flex;
+    gap: 50px;
+    padding: 62px 0;
+
+    span {
+      font-size: ${theme.fontSize.extra_large};
+      color: ${theme.colors.greenShades.g_60};
+    }
+  `}
 `
