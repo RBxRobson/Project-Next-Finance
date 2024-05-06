43bde67bec170724cc45117fae686ffea03a9641
RBxRobson <robsonnbarbosa0@gmail.com>
2024-05-06 17:24:21 -0300

    chore: Ajuste no tamanho da borda do tabSelector

--- src/components/TabSelector/styles.ts
+++ src/components/TabSelector/styles.ts
@@ -5,7 +5,7 @@ export const TabSelectorWrapper = styled.div`
     display: flex;
     width: fit-content;
     background-color: ${theme.colors.darkShades.d_11};
-    border: 1px solid ${theme.colors.darkShades.d_15};
+    border: 2px solid ${theme.colors.darkShades.d_15};
     padding: 14px 16px;
     border-radius: 82px;
 
