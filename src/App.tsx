87f8a4e97fd0205fdcfadde94626c416225fb673
RBxRobson <robsonnbarbosa0@gmail.com>
2024-05-08 14:43:14 -0300

    feat: Integra componente Home na aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,13 @@ import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './styles/global-styles'
 import theme from './styles/themes'
 
-import CtaSection from './components/CtaSection'
-import Footer from './components/Footer'
-import Hero from './components/Hero'
-import NavBar from './components/NavBar'
-import OurProducts from './components/OurProducts'
-import UseCases from './components/UseCases'
+import Home from './pages/Home'
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <NavBar />
-      <Hero />
-      <OurProducts />
-      <UseCases />
-      <CtaSection />
-      <Footer />
+      <Home />
     </ThemeProvider>
   )
 }
