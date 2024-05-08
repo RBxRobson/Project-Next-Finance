308928be511dfa5826258a1a8ac92f6753fa965a
RBxRobson <robsonnbarbosa0@gmail.com>
2024-05-08 14:42:46 -0300

    feat: Adiciona estrutura de paginas e cria o componente Home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,21 @@
+import CtaSection from '../../components/CtaSection'
+import Footer from '../../components/Footer'
+import Hero from '../../components/Hero'
+import NavBar from '../../components/NavBar'
+import OurProducts from '../../components/OurProducts'
+import UseCases from '../../components/UseCases'
+
+const Home = () => {
+  return (
+    <>
+      <NavBar />
+      <Hero />
+      <OurProducts />
+      <UseCases />
+      <CtaSection />
+      <Footer />
+    </>
+  )
+}
+
+export default Home
