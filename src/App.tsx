d2ee32ee832966f4d2ab3fe09702e3e9d52c8eac
RBxRobson <robsonnbarbosa0@gmail.com>
2024-05-09 15:42:22 -0300

    feat: Integra sistema de rotas no App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 
 import GlobalStyles from './styles/global-styles'
 import theme from './styles/themes'
 
-import Home from './pages/Home'
+import Routes from './routes'
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
