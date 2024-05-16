e2bd752dd3b9f717238cd146f3cb5b97ed123e37
RBxRobson <robsonnbarbosa0@gmail.com>
2024-05-16 17:24:33 -0300

    feat: Adiciona tipagem do serviço de API para Home

---
+++ src/types/response-home-api.d.ts
@@ -0,0 +1,41 @@
+declare type Product = {
+  id: number
+  icon: string
+  title: string
+  description: string
+}
+
+declare type Statistic = {
+  name: string
+  value: string
+}
+
+declare type Card = {
+  id: number
+  name: string
+  icon: string
+}
+
+declare type Home = {
+  hero: {
+    description: string
+  }
+  our_products: {
+    description: string
+    for_businesses: Product[]
+    for_people: Product[]
+  }
+  why_choose: {
+    description: string
+    for_people: {
+      description: string
+      statistics: Statistic[]
+      cards: Card[]
+    }
+    for_businesses: {
+      description: string
+      statistics: Statistic[]
+      cards: Card[]
+    }
+  }
+}
