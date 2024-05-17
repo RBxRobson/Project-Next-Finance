9ce8b74fff12cec36dbcc15d61c5d3ec2811264a
RBxRobson <robsonnbarbosa0@gmail.com>
2024-05-17 18:22:53 -0300

    feat: Adiciona o provider do redux na aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 
 import GlobalStyles from './styles/global-styles'
 import theme from './styles/themes'
 
+import { store } from './redux'
 import Routes from './routes'
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyles />
-      <BrowserRouter>
-        <Routes />
-      </BrowserRouter>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <BrowserRouter>
+          <Routes />
+        </BrowserRouter>
+      </ThemeProvider>
+    </Provider>
   )
 }
 
