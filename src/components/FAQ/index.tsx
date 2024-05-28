febf177b8ed11ca07e0319d6f835946cec0ed1b2
RBxRobson <robsonnbarbosa0@gmail.com>
2024-05-28 16:54:14 -0300

    feat: Adiciona conteúdo no componente FAQ

--- src/components/FAQ/index.tsx
+++ src/components/FAQ/index.tsx
@@ -2,15 +2,60 @@ import * as S from './styles'
 
 const FAQ = () => {
   return (
-    <S.HeaderFAQ className="container">
-      <h2>
-        Perguntas <span className="green-span">Frequentes</span>
-      </h2>
-      <p>
-        Você tem uma dúvida que não foi resolvida? Entre em contato com o
-        suporte pelo email: <a href="#">nextfinance@exemplo.com</a>
-      </p>
-    </S.HeaderFAQ>
+    <section className="container">
+      <S.HeaderFAQ>
+        <h2>
+          Perguntas <span className="green-span">Frequentes</span>
+        </h2>
+        <p>
+          Você tem uma dúvida que não foi resolvida? Entre em contato com o
+          suporte pelo email: <a href="#">nextfinance@exemplo.com</a>
+        </p>
+      </S.HeaderFAQ>
+      <S.CardsGrid>
+        <S.CardFAQ>
+          <h4>Como abro uma conta no Next Finance?</h4>
+          <p>
+            Abrir uma conta no Next Finance é fácil. Basta visitar nosso site e
+            clicar no botão &ldquo;Cadastrar-se&ldquo;. Siga as instruções,
+            forneça as informações necessárias e conclua o processo de
+            inscrição. Se você tiver alguma dúvida ou precisar de assistência,
+            nossa equipe de suporte ao cliente está disponível para ajudar.
+          </p>
+        </S.CardFAQ>
+        <S.CardFAQ>
+          <h4>Como abro uma conta no Next Finance?</h4>
+          <p>
+            Abrir uma conta no Next Finance é fácil. Basta visitar nosso site e
+            clicar no botão &ldquo;Cadastrar-se&ldquo;. Siga as instruções,
+            forneça as informações necessárias e conclua o processo de
+            inscrição. Se você tiver alguma dúvida ou precisar de assistência,
+            nossa equipe de suporte ao cliente está disponível para ajudar.
+          </p>
+        </S.CardFAQ>
+        <S.CardFAQ>
+          <h4>Como abro uma conta no Next Finance?</h4>
+          <p>
+            Abrir uma conta no Next Finance é fácil. Basta visitar nosso site e
+            clicar no botão &ldquo;Cadastrar-se&ldquo;. Siga as instruções,
+            forneça as informações necessárias e conclua o processo de
+            inscrição. Se você tiver alguma dúvida ou precisar de assistência,
+            nossa equipe de suporte ao cliente está disponível para ajudar.
+          </p>
+        </S.CardFAQ>
+        <S.CardFAQ>
+          <h4>Como abro uma conta no Next Finance?</h4>
+          <p>
+            Abrir uma conta no Next Finance é fácil. Basta visitar nosso site e
+            clicar no botão &ldquo;Cadastrar-se&ldquo;. Siga as instruções,
+            forneça as informações necessárias e conclua o processo de
+            inscrição. Se você tiver alguma dúvida ou precisar de assistência,
+            nossa equipe de suporte ao cliente está disponível para ajudar.
+          </p>
+        </S.CardFAQ>
+        <S.ButtonFAQ>Load All FAQ</S.ButtonFAQ>
+      </S.CardsGrid>
+    </section>
   )
 }
 
--- src/components/FAQ/styles.ts
+++ src/components/FAQ/styles.ts
@@ -1,7 +1,46 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 export const HeaderFAQ = styled.header`
   a {
     text-decoration: underline;
   }
+`
+
+export const CardsGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 30px;
+  padding: 80px 0 60px;
+  box-shadow: inset 0px -200px 100px -60px rgba(25, 25, 25, 1);
+  position: relative;
+`
+
+export const CardFAQ = styled.div`
+  ${({ theme }) => css`
+    border-radius: 14px;
+    border: 1px solid ${theme.colors.darkShades.d_15};
+    padding: 50px;
+    z-index: -1;
+
+    h4 {
+      border-bottom: 1px solid ${theme.colors.darkShades.d_15};
+      padding-bottom: 30px;
+      margin-bottom: 30px;
+    }
+  `}
+`
+
+export const ButtonFAQ = styled.button`
+  ${({ theme }) => css`
+    cursor: pointer;
+    position: absolute;
+    bottom: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    width: fit-content;
+    border: 1px solid ${theme.colors.darkShades.d_15};
+    background-color: ${theme.colors.darkShades.d_11};
+    padding: 18px;
+    border-radius: 100px;
+  `}
 `
