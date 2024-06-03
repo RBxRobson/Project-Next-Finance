981117a867997bb729d8b243422584c0c081e4d9
RBxRobson <robsonnbarbosa0@gmail.com>
2024-06-03 17:45:42 -0300

    feat: Adiciona arquivo index.ts para centralizar exportações da pasta images

---
+++ src/assets/images/index.ts
@@ -0,0 +1,26 @@
+import abstractDesign from './abstract_design_1.svg'
+import abstractDesignHero from './abstract_design_hero.svg'
+import bankLogo from './bank_logo.svg'
+import radialDesignLeft from './design_radius_lf.svg'
+import radialDesignRight from './design_radius_right.svg'
+import facebookIcon from './facebook_icon.png'
+import iconLocal from './icon_local.png'
+import iconMail from './icon_mail.png'
+import iconTelephone from './icon_telephone.png'
+import linkedinIcon from './linkedin_icon.png'
+import twitterIcon from './twitter_icon.png'
+import verifyIcon from './verify_icon.png'
+
+export {
+  iconLocal,
+  iconMail,
+  iconTelephone,
+  linkedinIcon,
+  facebookIcon,
+  verifyIcon,
+  twitterIcon,
+  bankLogo,
+  abstractDesignHero,
+  radialDesignRight,
+  radialDesignLeft
+}
