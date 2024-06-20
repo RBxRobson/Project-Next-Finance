024e80d427011ee7d87ceb82f1e9383febcf08d1
RBxRobson <robsonnbarbosa0@gmail.com>
2024-06-20 17:39:41 -0300

    feat: Faz a integração do swiper

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { register } from 'swiper/element/bundle'
 
 import App from './App'
+
+register()
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/scrollbar'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
