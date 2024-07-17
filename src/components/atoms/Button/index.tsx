7085b6fffce8eeb88ae80377ad3bc1856e9baeed
RBxRobson <robsonnbarbosa0@gmail.com>
2024-07-17 14:49:01 -0300

    refactor: Melhora a lógica de renderização do Button

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,32 +1,51 @@
-import theme from '../../../styles/themes'
+import { motion } from 'framer-motion'
+import { Link } from 'react-router-dom'
 
 import * as S from './style'
 
-type Props = {
+type ButtonProps = {
   buttonText: string
-  type: 'primary' | 'secondary'
-  className?: string
+  type: 'btn_green' | 'btn_green--lg' | 'link_green'
+  className?: 'btn__register' | 'btn--disabled' | ''
   onClick?: () => void
+  link?: string
 }
 
-const Button = ({ buttonText, type, className, onClick }: Props) => {
-  return type === 'primary' ? (
-    <S.ButtonPrimary
-      className={className}
-      whileTap={{ scale: 1.1 }}
-      whileHover={{ filter: 'brightness(0.9)' }}
-    >
-      {buttonText}
-    </S.ButtonPrimary>
-  ) : (
-    <S.ButtonSecondary
-      className={className}
-      onClick={onClick}
-      whileTap={{ scale: 1.1 }}
-    >
-      {buttonText}
-    </S.ButtonSecondary>
-  )
+const Button = ({
+  buttonText,
+  type,
+  className,
+  onClick,
+  link
+}: ButtonProps) => {
+  const MotionLink = motion(Link)
+
+  const attrButton = {
+    className,
+    whileTap: { scale: 1.1 },
+    whileHover: { filter: 'brightness(0.9)' },
+    onClick
+  }
+
+  if (type === 'link_green' && link) {
+    return (
+      <S.BtnGreen as={MotionLink} to={link} {...attrButton}>
+        {buttonText}
+      </S.BtnGreen>
+    )
+  } else if (type === 'btn_green--lg') {
+    return (
+      <S.BtnGreenLarge as={motion.button} {...attrButton}>
+        {buttonText}
+      </S.BtnGreenLarge>
+    )
+  } else if (type === 'btn_green') {
+    return (
+      <S.BtnGreen as={motion.button} {...attrButton}>
+        {buttonText}
+      </S.BtnGreen>
+    )
+  }
 }
 
 export default Button
--- src/components/atoms/Button/style.ts
+++ src/components/atoms/Button/style.ts
@@ -1,25 +1,42 @@
 import { motion } from 'framer-motion'
 import styled, { css } from 'styled-components'
 
-export const ButtonPrimary = styled(motion.button)`
+import { breakpoint } from '../../../styles/themes'
+
+const Button = styled.button`
   ${({ theme }) => css`
     border: none;
     cursor: pointer;
     white-space: nowrap;
-    border-radius: 82px;
-    padding: 14px 30px;
     font-weight: ${theme.fontWeight.regular};
-    background-color: ${theme.colors.greenShades.g_60};
-    color: ${theme.colors.darkShades.d_11};
 
-    &.is--deactivated {
+    &.btn--disabled,
+    &.btn__register {
       background-color: transparent;
       color: ${theme.colors.absoluteColors.white};
+    }
+
+    &.btn__register {
+      padding-left: 0;
+    }
+
+    // Breakpoint Desktops 1200px
+    ${breakpoint(theme.breakpoints.xl)} {
+      font-size: ${theme.fontSize.xxs};
     }
   `}
 `
 
-export const ButtonSecondary = styled(ButtonPrimary)`
+export const BtnGreen = styled(Button)`
+  ${({ theme }) => css`
+    border-radius: 82px;
+    padding: 14px 30px;
+    background-color: ${theme.colors.greenShades.g_60};
+    color: ${theme.colors.darkShades.d_11};
+  `}
+`
+
+export const BtnGreenLarge = styled(BtnGreen)`
   border-radius: 140px;
   padding: 14px 24px;
 `
