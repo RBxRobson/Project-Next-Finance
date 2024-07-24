71ab1b3270d4685f83d395a21de4f6d17c070149
RBxRobson <robsonnbarbosa0@gmail.com>
2024-07-24 17:00:50 -0300

    refactor: Reaproveita componentes usados em múltiplas paginas, em uma mesma rota, para evitar re-renderizações

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,38 @@
 import { Route, Routes as RouterRoutes } from 'react-router-dom'
 
+import { Footer } from '../components/organisms'
+import Header from '../components/templates/Header'
 import Auth from '../pages/Auth'
 import Home from '../pages/Home'
+
+type LayoutProps = {
+  children: JSX.Element
+}
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  )
+}
 
 const Routes = () => {
   return (
     <RouterRoutes>
-      <Route path="/" element={<Home />} />
       <Route path="/auth" element={<Auth />} />
+      <Route
+        path="/*"
+        element={
+          <Layout>
+            <RouterRoutes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<Auth />} />
+            </RouterRoutes>
+          </Layout>
+        }
+      />
     </RouterRoutes>
   )
 }
