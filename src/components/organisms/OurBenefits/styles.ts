06a096aed2f83f2dbdb942aeda1669285334dcd9
RBxRobson <robsonnbarbosa0@gmail.com>
2024-07-31 15:26:33 -0300

    feat: Adiciona responsividade ao componente OurBenefits

--- src/components/organisms/OurBenefits/styles.ts
+++ src/components/organisms/OurBenefits/styles.ts
@@ -7,8 +7,14 @@ export const BenefitsCards = styled.ul`
     display: grid;
     grid-template-columns: 1fr 1fr;
     margin-top: 80px;
-    grid-gap: 1px;
+    gap: 1px;
     background-color: ${theme.colors.darkShades.d_15};
+
+    // Breakpoint Desktop 992px
+    ${breakpoint(theme.breakpoints.lg)} {
+      grid-template-columns: 1fr;
+      gap: 0;
+    }
   `}
 `
 
@@ -34,6 +40,17 @@ export const CardContainer = styled.li`
     &:nth-last-child(2) {
       padding-bottom: 0;
     }
+
+    // Breakpoint Desktop 992px
+    ${breakpoint(theme.breakpoints.lg)} {
+      text-align: start;
+      padding: 0;
+      padding-bottom: 30px;
+
+      &:nth-last-child(2) {
+        padding-bottom: 30px;
+      }
+    }
   `}
 `
 
@@ -45,6 +62,16 @@ export const Card = styled.div`
     background-color: ${theme.colors.darkShades.d_11};
     border: 1px solid ${theme.colors.darkShades.d_15};
     border-radius: 50px 20px;
+
+    // Breakpoint Desktop 992px
+    ${breakpoint(theme.breakpoints.lg)} {
+      padding: 40px;
+    }
+
+    // Breakpoint Desktop 576px
+    ${breakpoint(theme.breakpoints.sm)} {
+      padding: 24px;
+    }
 
     &::after {
       content: '';
@@ -74,10 +101,22 @@ export const CardTitle = styled.div`
     img {
       width: 80px;
       height: 80px;
+
+      // Breakpoint Desktop 576px
+      ${breakpoint(theme.breakpoints.sm)} {
+        width: 70px;
+        height: 70px;
+      }
     }
+
     h3 {
       font-size: ${theme.fontSize.lg};
       font-weight: ${theme.fontWeight.regular};
+
+      // Breakpoint Desktop 576px
+      ${breakpoint(theme.breakpoints.sm)} {
+        font-size: ${theme.fontSize.xs};
+      }
     }
   `}
 `
