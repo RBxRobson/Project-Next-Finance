4a59963361fefbb6b3c1db4a65f8729c83b0d06f
RBxRobson <robsonnbarbosa0@gmail.com>
2024-08-03 14:10:51 -0300

    feat: Adiciona responsividade ao componente JobOpenings

--- src/components/organisms/JobOpenings/styles.ts
+++ src/components/organisms/JobOpenings/styles.ts
@@ -1,10 +1,19 @@
 import styled, { css } from 'styled-components'
 
+import { breakpoint } from '../../../styles/themes'
+
 export const Vacancies = styled.ul`
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 30px;
-  margin-top: 80px;
+  ${({ theme }) => css`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 30px;
+    margin-top: 80px;
+
+    // Breakpoint Desktop 992px
+    ${breakpoint(theme.breakpoints.lg)} {
+      grid-template-columns: 1fr;
+    }
+  `}
 `
 
 export const Vacancy = styled.li`
@@ -15,12 +24,25 @@ export const Vacancy = styled.li`
     border: 1px solid ${theme.colors.darkShades.d_15};
     border-radius: 16px;
     padding: 50px;
+    text-align: start;
+
+    // Breakpoint Desktop 576px
+    ${breakpoint(theme.breakpoints.sm)} {
+      padding: 24px;
+    }
 
     h3,
     h4 {
       font-size: ${theme.fontSize.lgmd};
       font-weight: ${theme.fontWeight.semi_bold};
       margin-bottom: 22px;
+    }
+
+    h3 {
+      // Breakpoint Desktop 576px
+      ${breakpoint(theme.breakpoints.sm)} {
+        text-align: center;
+      }
     }
 
     h4 {
@@ -30,6 +52,12 @@ export const Vacancy = styled.li`
     button {
       width: fit-content;
       margin-top: auto;
+
+      // Breakpoint Desktop 576px
+      ${breakpoint(theme.breakpoints.sm)} {
+        width: 90%;
+        margin: 0 auto;
+      }
     }
   `}
 `
@@ -39,10 +67,17 @@ export const Description = styled.p`
 `
 
 export const TagsContainer = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 10px;
-  margin-bottom: 50px;
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+    gap: 10px;
+    margin-bottom: 50px;
+
+    // Breakpoint Desktop 576px
+    ${breakpoint(theme.breakpoints.sm)} {
+      flex-direction: column;
+    }
+  `}
 `
 
 export const Tag = styled.span`
