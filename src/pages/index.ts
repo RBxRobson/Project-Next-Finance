60853dd3ab814cbd02d9b08a9b34c45227cb492d
RBxRobson <robsonnbarbosa0@gmail.com>
2024-08-14 10:17:49 -0300

    feat: Adiciona arquivo de exportação para pasta pages

---
+++ src/pages/index.ts
@@ -0,0 +1,5 @@
+export { default as About } from './About'
+export { default as Auth } from './Auth'
+export { default as Careers } from './Careers'
+export { default as Home } from './Home'
+export { default as Security } from './Security'
